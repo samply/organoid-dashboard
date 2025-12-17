42f76b0d330bd02cfbf4168917e90265a268eef7
Tim Schumacher <tim.schumacher@dkfz-heidelberg.de>
2025-12-17 13:14:12 +0100

    Support XML FHIR bundles in test env

--- dev/container-setup-scripts/blaze-loader.sh
+++ dev/container-setup-scripts/blaze-loader.sh
@@ -1,2 +1,31 @@
-wget -qO - https://github.com/samply/blazectl/releases/download/v0.17.0/blazectl-0.17.0-linux-amd64.tar.gz | tar -xzf - blazectl
-./blazectl --server http://blaze:8080/fhir upload json-fhir-bundles
+#!/bin/sh
+
+# Install curl if not available
+apk add --no-cache curl
+
+BLAZE_URL="http://blaze:8080/fhir"
+BUNDLE_DIR="fhir-bundles"
+
+echo "Starting to upload FHIR bundles to $BLAZE_URL"
+
+# Upload all JSON files
+echo "Uploading JSON bundles..."
+for file in "$BUNDLE_DIR"/*.json; do
+  if [ -f "$file" ]; then
+    echo "Uploading $file"
+    curl -X POST -H "Content-Type: application/fhir+json" -d @"$file" "$BLAZE_URL"
+    echo ""
+  fi
+done
+
+# Upload all XML files
+echo "Uploading XML bundles..."
+for file in "$BUNDLE_DIR"/*.xml; do
+  if [ -f "$file" ]; then
+    echo "Uploading $file"
+    curl -X POST -H "Content-Type: application/fhir+xml" -d @"$file" "$BLAZE_URL"
+    echo ""
+  fi
+done
+
+echo "Upload complete!"
